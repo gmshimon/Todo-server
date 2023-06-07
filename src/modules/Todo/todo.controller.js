9ea42a93b505a03e245a62fc5770f4901e73bb59
gmshimon <simon.rosedale99@gmail.com>
2023-06-07 21:51:24 +0800

    update todo done

--- src/modules/Todo/todo.controller.js
+++ src/modules/Todo/todo.controller.js
@@ -1,36 +1,74 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../../../utilis/dbConfig");
 
-module.exports.getTodos=async(req,res)=>{
-    try {
-        const db = getDb();
-        const result = await db.collection('todos').find().toArray();
+module.exports.getTodos = async (req, res) => {
+  try {
+    const db = getDb();
+    const result = await db.collection("todos").find().toArray();
 
-        if(result.length == 0) {
-            return res.status(400).json({success:false,error:"No data"})
-        }
-        res.status(200).json({success:true,data:result})
-    } catch (error) {
-        res.status(400).json({
-            status:false,
-            message:error.message
-        })
+    if (result.length == 0) {
+      return res.status(400).json({ success: false, error: "No data" });
     }
-}
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
 
-module.exports.postTodo = async(req,res)=>{
-    try {
-        const db = getDb();
-        const data = req.body;
-        const result = await db.collection("todos").insertOne(data)
+module.exports.deleteTodo = async (req, res) => {
+  try {
+    const db = getDb();
+    const id = req.params.id;
+    const result = await db
+      .collection("todos")
+      .deleteOne({ _id: new ObjectId(id) });
 
-        res.status(200).json({
-            status:true,
-            message:"data saved successfully"
-        })
-    } catch (error) {
-        res.status(400).json({
-            status:false,
-            message:error.message
-        })
+    res.status(200).json({
+      success: true,
+      message: "Deleted successfully",
+    });
+  } catch (error) {}
+};
+
+module.exports.updateTodo = async (req, res) => {
+  try {
+    const db = getDb();
+    const id = req.params.id;
+    const body = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+    const updateDoc = {
+        $set:body
     }
-}
+    console.log(updateDoc);
+    const result = await db
+      .collection("todos")
+      .updateOne(filter, updateDoc,options);
+
+      res.status(200).json({
+        success: true,
+        message: "Updated successfully",
+      })
+  } catch (err) {}
+};
+
+module.exports.postTodo = async (req, res) => {
+  try {
+    const db = getDb();
+    const data = req.body;
+    const result = await db.collection("todos").insertOne(data);
+
+    res.status(200).json({
+      status: true,
+      message: "data saved successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
--- src/modules/Todo/todo.router.js
+++ src/modules/Todo/todo.router.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { postTodo, getTodos } = require("./todo.controller");
+const { postTodo, getTodos, deleteTodo, updateTodo } = require("./todo.controller");
 const router = express.Router();
 
 router.route('/').get(getTodos).post(postTodo)
+router.route('/:id').get(deleteTodo).put(updateTodo)
 
 module.exports = router;
