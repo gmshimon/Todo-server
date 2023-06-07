c0465be6b25f3651f396591216d586f023dcb7ed
gmshimon <simon.rosedale99@gmail.com>
2023-06-07 21:18:07 +0800

    get todo api

--- src/modules/Todo/todo.controller.js
+++ src/modules/Todo/todo.controller.js
@@ -1,7 +1,20 @@
 const { getDb } = require("../../../utilis/dbConfig");
 
 module.exports.getTodos=async(req,res)=>{
-    res.send("get todos");
+    try {
+        const db = getDb();
+        const result = await db.collection('todos').find().toArray();
+
+        if(result.length == 0) {
+            return res.status(400).json({success:false,error:"No data"})
+        }
+        res.status(200).json({success:true,data:result})
+    } catch (error) {
+        res.status(400).json({
+            status:false,
+            message:error.message
+        })
+    }
 }
 
 module.exports.postTodo = async(req,res)=>{
